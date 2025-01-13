e7a1e98eaec016e27e13de825579facb27803748
Duany Espindola <duanyespindola@gmail.com>
2025-01-13 10:05:50 -0300

    First example in https://go.dev/blog/strings

---
+++ 004-runes/go.mod
@@ -0,0 +1,3 @@
+module example/runes
+
+go 1.23.4
---
+++ 004-runes/runes.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mystring = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+	var sample = []byte(mystring)
+	// var sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+
+	fmt.Println("Println:")
+	fmt.Println(sample)
+
+	fmt.Println("Byte loop:")
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("%x ", sample[i])
+	}
+	fmt.Printf("\n")
+
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("%q ", sample[i])
+	}
+	fmt.Printf("\n")
+
+	fmt.Println("Printf with %x:")
+	fmt.Printf("%x\n", sample)
+
+	fmt.Println("Printf with % x:")
+	fmt.Printf("% x\n", sample)
+
+	fmt.Println("Printf with %q:")
+	fmt.Printf("%q\n", sample)
+
+	fmt.Println("Printf with %+q:")
+	fmt.Printf("%+q\n", sample)
+}
